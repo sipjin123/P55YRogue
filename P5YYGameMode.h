f170da6f19f2f7307deab52823752d78c96a853d
Dev_Burlin <burlin_gauna@yahoo.com>
2023-04-07 14:26:12 +0800

    initial commit of test scripts for ue5 studies

---
+++ P5YYGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "P5YYGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AP5YYGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AP5YYGameMode();
+};
+
+
+
