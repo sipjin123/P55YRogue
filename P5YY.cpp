13e466819fed070d6be91800591b435b53d72aca
Dev_Burlin <burlin_gauna@yahoo.com>
2023-04-07 15:08:13 +0800

    removed test scripts

--- P5YY.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "P5YY.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, P5YY, "P5YY" );
- 
