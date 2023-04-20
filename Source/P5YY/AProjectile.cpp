aec1b365fe62e552b828e4d6db156a5c098739a7
Dev_Burlin <burlin_gauna@yahoo.com>
2023-04-20 19:58:50 +0800

    setup projectile w/ projectile movement compponent, added public function to allow orientation & location initialization

---
+++ Source/P5YY/AProjectile.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AProjectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+#include "Engine/StaticMesh.h"
+
+// Sets default values
+AAProjectile::AAProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    SceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+    RootComponent = SceneComponent;
+
+    CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("SphereCollider"));
+    CollisionSphere->SetSphereRadius(15);
+    CollisionSphere->SetCollisionProfileName("SphereCollision");
+    CollisionSphere->SetupAttachment(RootComponent);
+
+    StaticMeshSource = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StatMeshComp"));
+    StaticMeshSource->SetVisibility(true);
+    StaticMeshSource->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+
+    if (!ProjectileMovementComponent)
+    {
+        // Use this component to drive this projectile's movement.
+        ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+        ProjectileMovementComponent->SetUpdatedComponent(RootComponent);
+        ProjectileMovementComponent->InitialSpeed = 1500.0f;
+        ProjectileMovementComponent->MaxSpeed = 1500.0f;
+        ProjectileMovementComponent->bRotationFollowsVelocity = true;
+        ProjectileMovementComponent->bShouldBounce = true;
+        ProjectileMovementComponent->Bounciness = 0.3f;
+        ProjectileMovementComponent->ProjectileGravityScale = 0.0f;
+    }
+}
+
+// Called when the game starts or when spawned
+void AAProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AAProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AAProjectile::InitializeProjectile(FVector& newDirection, FVector& spawnPoint, FRotator& startRotator) {
+
+    SetActorLocation(spawnPoint);
+    SetActorRotation(startRotator);
+    ProjectileMovementComponent->Velocity = newDirection * ProjectileMovementComponent->InitialSpeed;
+}
---
+++ Source/P5YY/AProjectile.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AProjectile.generated.h"
+
+UCLASS()
+class P5YY_API AAProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AAProjectile();
+
+	// Sphere collision component.
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Collision")
+		class USphereComponent* CollisionSphere;
+
+	// Projectile movement component.
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		class UProjectileMovementComponent* ProjectileMovementComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Display")
+		class UStaticMeshComponent* StaticMeshSource;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		class USceneComponent* SceneComponent;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Properties")
+	void InitializeProjectile(FVector& newDirection, FVector& spawnPoint, FRotator& startRotator);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
