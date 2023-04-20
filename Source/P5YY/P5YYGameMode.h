676a45f181159d2957329e1aefdcced1d2a9df51
Dev_Burlin <burlin_gauna@yahoo.com>
2023-04-20 19:58:15 +0800

    setup external class referencing for game mode script

--- Source/P5YY/P5YYGameMode.h
+++ Source/P5YY/P5YYGameMode.h
@@ -4,15 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+//#include "ASpawnableObj.h"
+//#include "Prototypes/AGenTestActor.h"
 #include "P5YYGameMode.generated.h"
 
-UCLASS(minimalapi)
+UCLASS()//UCLASS(minimalapi)
 class AP5YYGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
 
 public:
 	AP5YYGameMode();
+
+	UFUNCTION(BlueprintCallable, Category = "Group1")
+		virtual void UpdateGameModeNewX();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		TSubclassOf<APawn> CustomChar;
 };
 
 
