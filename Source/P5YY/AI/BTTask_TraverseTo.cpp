f5fe8da3f45afca8147ad8d334d6ff6dc71033fd
Dev_Burlin <burlin_gauna@yahoo.com>
2023-04-26 00:36:15 +0800

    - added new custom bt task w/c will be used for ai traversal - setup blueprint task nodes to lerp rotation and execute ai move to target - added tick service to node for interpolation

---
+++ Source/P5YY/AI/BTTask_TraverseTo.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTTask_TraverseTo.h"
+
+void UBTTask_TraverseTo::OnGameplayTaskInitialized(UGameplayTask& Task) {
+
+}
+
+void UBTTask_TraverseTo::OnGameplayTaskActivated(UGameplayTask& Task) {
+
+}
+
+void UBTTask_TraverseTo::OnGameplayTaskDeactivated(UGameplayTask& Task) {
+
+}
