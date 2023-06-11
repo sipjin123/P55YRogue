8a447d4ba5f7c55ee0d867e95f7e2edad3957ffd
sipjin123 <burlin_gauna@yahoo.com>
2023-06-11 22:34:27 +0800

    used smart svn for commit

--- Source/P5YY/AProjectile.h
+++ Source/P5YY/AProjectile.h
@@ -40,7 +40,7 @@ public:
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Stats")
 		bool ExplodesOnCollision;
-	// Test for Tortoise
+	// Test for SmartSvn
 	// Test from Sourcetree
 public:
 	UFUNCTION(BlueprintCallable, Category = "Properties")
