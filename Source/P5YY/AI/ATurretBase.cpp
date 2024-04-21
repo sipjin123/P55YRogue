7bc24e91c38ef3aaed04d7a37cee7d8552dcaef7
burlin_gauna <burlin_gauna@yahoo.com>
2024-04-21 20:40:18 +0800

    setup working prototype for basic mining functionality and tower targeting system / minor adjustments for shaders

---
+++ Source/P5YY/AI/ATurretBase.cpp
@@ -0,0 +1,21 @@
+﻿#include "ATurretBase.h"
+#include "Engine/StaticMesh.h"
+
+ATurretBase::ATurretBase()
+{
+	StatMeshSource = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StatMeshComp"));
+	StatMeshSource->SetVisibility(true);
+	StatMeshSource->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
+}
+
+// Called when the game starts or when spawned
+void ATurretBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void ATurretBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
