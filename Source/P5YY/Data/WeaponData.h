6fe2ffda061f15e0443b9afe9c508409b8b8aa6b
burlin_gauna <burlin_gauna@yahoo.com>
2024-06-10 11:18:49 +0800

    initial setup for weapon data table class and blueprints

---
+++ Source/P5YY/Data/WeaponData.h
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "WeaponData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWeaponData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText Name;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float BaseDamage;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float Ammo;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	float RateOfFire;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSoftObjectPtr<UStaticMesh> WeaponModel;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSoftClassPtr<AActor> ActorClass;
+};
+
+// Note: Use soft reference for object and classes only
