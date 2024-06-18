1cf55bbaa690ca2202efa3677bcb0bdfa7ba775e
burlin_gauna <burlin_gauna@yahoo.com>
2024-06-19 06:09:38 +0800

    initial setup for item data class and struct

---
+++ Source/P5YY/Data/ItemBase.cpp
@@ -0,0 +1,1 @@
+﻿#include "ItemBase.h"
---
+++ Source/P5YY/Data/ItemBase.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "ItemBase.generated.h"
+
+UCLASS()
+class UItemBase : public  UObject
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(VisibleAnywhere, Category="Item Data")
+	int32 Id;
+	UPROPERTY(VisibleAnywhere, Category="Item Data", meta=(UIMin=1, UIMax=100))
+	int32 Quantity;
+};
