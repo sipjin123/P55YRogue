a938710e6c69a5e36856b42bb11f3d8c2a800c38
burlin_gauna <burlin_gauna@yahoo.com>
2024-06-19 16:12:40 +0800

    updated item base class for in game usage

--- Source/P5YY/Data/ItemBase.cpp
+++ Source/P5YY/Data/ItemBase.cpp
@@ -1,1 +1,30 @@
 ﻿#include "ItemBase.h"
+
+UItemBase::UItemBase()
+{
+}
+
+UItemBase* UItemBase::CreateItemCopy() const
+{
+	UItemBase* ItemCopy = NewObject<UItemBase>(StaticClass());
+	ItemCopy->Id = this->Id;
+	ItemCopy->Id = this->Id;
+	
+	ItemCopy->ItemRarity = this->ItemRarity;
+	ItemCopy->ItemTextData = this->ItemTextData;
+	ItemCopy->ItemType = this->ItemType;
+	ItemCopy->ItemStats = this->ItemStats;
+	ItemCopy->ItemAssetData = this->ItemAssetData;
+	ItemCopy->ItemStorageInfo = this->ItemStorageInfo;
+
+	return ItemCopy;
+}
+
+void UItemBase::SetQuanity(int32 NewQuantity)
+{
+	Quantity = FMath::Clamp(NewQuantity, 0 , ItemStorageInfo.IsStackable ? ItemStorageInfo.MaxStackSize : 1);
+	if (Quantity <= 0)
+	{
+		// Remove from inventory
+	}
+}
--- Source/P5YY/Data/ItemBase.h
+++ Source/P5YY/Data/ItemBase.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include "ItemData.h"
 #include "ItemBase.generated.h"
 
 UCLASS()
@@ -10,4 +11,27 @@ public:
 	int32 Id;
 	UPROPERTY(VisibleAnywhere, Category="Item Data", meta=(UIMin=1, UIMax=100))
 	int32 Quantity;
+	
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	EItemRarity ItemRarity;
+	UPROPERTY(VisibleAnywhere, Category="Item Data")
+	FItemTextData ItemTextData;
+	UPROPERTY(EditAnywhere, Category="Item Data")
+	EItemType ItemType;
+	UPROPERTY(VisibleAnywhere, Category="Item Data")
+	FItemStats ItemStats;
+	UPROPERTY(VisibleAnywhere, Category="Item Data")
+	FItemAssetData ItemAssetData;
+	UPROPERTY(VisibleAnywhere, Category="Item Data")
+	FItemStorageInfo ItemStorageInfo;
+	
+public:
+	UItemBase();
+	UItemBase* CreateItemCopy() const;
+
+	FORCEINLINE float GetItemStackWeight() const { return Quantity * ItemStorageInfo.Weight; };
+	FORCEINLINE float GetItemSingleWeight() const { return ItemStorageInfo.Weight; };
+	FORCEINLINE bool IsFullStack() const { return ItemStorageInfo.MaxStackSize == Quantity; };
+
+	void SetQuanity(int32 NewQuantity);
 };
