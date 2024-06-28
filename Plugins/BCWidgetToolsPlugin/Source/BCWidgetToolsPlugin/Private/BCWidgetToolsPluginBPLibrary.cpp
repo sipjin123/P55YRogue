9de51a1c54db1abfd91e47ff0d8b1757858e2147
burlin_gauna <burlin_gauna@yahoo.com>
2024-06-28 11:46:51 +0800

    initial setup for custom plugin for widgets

---
+++ Plugins/BCWidgetToolsPlugin/Source/BCWidgetToolsPlugin/Private/BCWidgetToolsPluginBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BCWidgetToolsPluginBPLibrary.h"
+#include "BCWidgetToolsPlugin.h"
+
+UBCWidgetToolsPluginBPLibrary::UBCWidgetToolsPluginBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UBCWidgetToolsPluginBPLibrary::BCWidgetToolsPluginSampleFunction(float Param)
+{
+	return -1;
+}
+
