1dc7aa7b06e896b12d98ace4bf93bf9cc55c5990
burlin_gauna <burlin_gauna@yahoo.com>
2024-07-13 20:13:31 +0800

    added new subsystem scripts and blueprints / setup new maps for testing and prototyping features for future use

---
+++ Source/P5YY/Subsystems/CustomGameInstanceSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "CustomGameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class P5YY_API UCustomGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
