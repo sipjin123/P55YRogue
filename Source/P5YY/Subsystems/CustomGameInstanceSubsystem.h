061d11dfc2034499b811eb3219a7cca85054d984
burlin_gauna <burlin_gauna@yahoo.com>
2024-07-13 20:14:05 +0800

    updated custo mgame instance to be used in blueprints

--- Source/P5YY/Subsystems/CustomGameInstanceSubsystem.h
+++ Source/P5YY/Subsystems/CustomGameInstanceSubsystem.h
@@ -3,15 +3,34 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "P5YY/Data/InventoryTrackerDA.h"
 #include "Subsystems/GameInstanceSubsystem.h"
 #include "CustomGameInstanceSubsystem.generated.h"
 
 /**
  * 
  */
-UCLASS()
+
+DECLARE_LOG_CATEGORY_EXTERN(InstanceSubsystemLog, Log, All);
+UCLASS(Abstract, Blueprintable)
 class P5YY_API UCustomGameInstanceSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(BlueprintReadWrite)
+	int TestInt = 0;
+public:
+	UFUNCTION(BlueprintCallable)
+	void TestFunc();
+
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	UFUNCTION(BlueprintImplementableEvent)
+	// ReSharper disable once CppUEBlueprintImplementableEventNotImplemented
+	void InitEventCalledInBlueprint();
 	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TObjectPtr<const UInventoryTrackerDA> InventoryTrackerDA;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	TAssetSubclassOf<UInventoryTrackerDA> InventoryTrackerDASub;
 };
