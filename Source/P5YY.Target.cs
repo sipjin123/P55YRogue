4831926d62e15482d5241b5f677a745f1388b028
burlin_gauna <burlin_gauna@yahoo.com>
2024-11-15 18:37:16 +0800

    updated targets to allow compiling after version switch to UE5.5

--- Source/P5YY.Target.cs
+++ Source/P5YY.Target.cs
@@ -8,8 +8,11 @@ public class P5YYTarget : TargetRules
 	public P5YYTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
-		ExtraModuleNames.Add("P5YY");
+        //BuildEnvironment = TargetBuildEnvironment.Unique;
+        DefaultBuildSettings = BuildSettingsVersion.V4;
+        //DefaultBuildSettings = BuildSettingsVersion.V2;
+        //IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
+        ExtraModuleNames.Add("P5YY");
 	}
 }
